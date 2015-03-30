c499c57b09c725de8cdb2c42236170f18c42522b
Maciej Iwanowski <kasztelix@gmail.com>
2015-03-30 22:00:45 +0200

    adding priority and message setting capabilities; initiating slice to start at the beginning of an array

--- parsers/cpd.go
+++ parsers/cpd.go
@@ -4,12 +4,13 @@ package parsers
 
 import "encoding/xml"
 import "github.com/dzielne-misie/ral/violations"
+import "fmt"
 
 type Cpd struct {
 }
 
 func (cpd *Cpd) Parse(f Decoder) (v []violations.Violation, err error) {
-	v = make([]violations.Violation, 100, 500)
+	v = make([]violations.Violation, 0, 500)
 	err = nil
 
 	for {
@@ -21,12 +22,14 @@ func (cpd *Cpd) Parse(f Decoder) (v []violations.Violation, err error) {
 		case xml.StartElement:
 			if se.Name.Local == "duplication" {
 				var dup violations.Duplication
-				f.DecodeElement(dup, &se)
+				f.DecodeElement(&dup, &se)
 				violation := new(violations.Violation)
 				violation.Type = "cpd"
+				violation.Priority = 1
+				violation.Message = fmt.Sprintf("%d duplicated lines and %d duplicated tokens", dup.Lines, dup.Tokens)
+				v = append(v, *violation)
 			}
 		}
-		break
 	}
 
 	return v, err
